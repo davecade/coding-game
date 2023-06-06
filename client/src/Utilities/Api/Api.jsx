cdd7b5962b09e1293be294346e58c7b463ae275a
Dave <dave.cadelina@outlook.com>
2023-06-06 11:24:26 +1000

    refactored api file with axios.create to change between environments

--- client/src/Utilities/Api/Api.jsx
+++ client/src/Utilities/Api/Api.jsx
@@ -1,48 +1,45 @@
 import axios from 'axios';
 
-let baseURL;
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001'
+    : 'https://mysterious-meadow-53796.herokuapp.com';
 
-if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:3001';
-} else {
-  baseURL = 'https://mysterious-meadow-53796.herokuapp.com';
-}
+// Create axios request with the base URL
+const request = axios.create({ baseURL });
 
 export const getQuestions = async () => {
   try {
-    const response = await axios.get(baseURL + '/questions');
+    const response = await request.get('/questions');
     return response.data;
   } catch (e) {
-    console.log(e);
+    throw new Error(`Failed to get questions: ${e}`);
   }
 };
 
 export const submitCode = async (questionId, solution) => {
   try {
-    const response = await axios.post(baseURL + '/submit', {
-      questionId,
-      solution,
-    });
+    const response = await request.post('/submit', { questionId, solution });
     return response.data;
-  } catch (error) {
-    console.error(error);
+  } catch (e) {
+    throw new Error(`Failed to submit code: ${e}`);
   }
 };
 
 export const getJdoodleToken = async () => {
   try {
-    const response = await axios.get(baseURL + '/token');
+    const response = await request.get('/token');
     return response.data;
-  } catch (error) {
-    console.error(error);
+  } catch (e) {
+    throw new Error(`Failed to get Jdoodle token: ${e}`);
   }
 };
 
 export const resetCode = async (id) => {
   try {
-    const response = await axios.patch(baseURL + '/reset' + `/${id}`);
+    const response = await request.patch(`/reset/${id}`);
     return response.data;
-  } catch (error) {
-    console.error(error);
+  } catch (e) {
+    throw new Error(`Failed to reset code: ${e}`);
   }
 };
